f68ef94480f795ea19565ff3fd4a36e10c6bd29d
opilane <opilane@HP-CZC6297BB4>
2022-01-28 15:24:51 +0200

    Add project files.

---
+++ admin.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyVorm
+{
+   
+    class admin: System.Windows.Forms.Form
+    {
+        static string connect_KinoDB = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\opilane\source\repos\Kinosaal23-master\AppData\Database1.mdf;Integrated Security=True";
+        public SqlConnection connect_to_DB = new SqlConnection(connect_KinoDB);
+
+        SqlCommand command;
+        SqlDataAdapter adapter;
+        public admin()
+        {
+            this.Size = new System.Drawing.Size(600, 800);
+            this.BackgroundImage = new Bitmap(@"../../image/kos.jpg");
+            Button btn = new Button()
+            {
+                Size = new Size(75, 75),
+                Location = new Point(20, 30),
+                Text = "vaata tabel"
+            };
+            btn.MouseClick += Btn_MouseClick;
+            this.Controls.Add(btn);
+            Button filmiAdd = new Button()
+            {
+                Size = new Size(75, 75),
+                Location = new Point(220,300),
+                Text = "Lisa film"
+            };
+            btn.MouseClick += Btn_MouseClick;
+            this.Controls.Add(filmiAdd);
+            filmiAdd.MouseClick += FilmiAdd_MouseClick;
+
+        }
+
+        private void FilmiAdd_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (name.Text != "" && date.Text != "" && genre.Text != "" && posters.Image != null)
+            {
+                command = new SqlCommand("UPDATE Movie  SET name=@name,date=@date,genre=@genre, image=@image WHERE ID_movie=@ID_movie", connect_to_DB);
+                connect_to_DB.Open();
+                command.Parameters.AddWithValue("@ID_movie", id);
+                command.Parameters.AddWithValue("@name", name.Text);
+                command.Parameters.AddWithValue("@date", date.Text);
+                command.Parameters.AddWithValue("@genre", genre.Text.Replace(",", "."));
+                string file_pilt = image.Text + ".jpg";
+                command.Parameters.AddWithValue("@image", file_pilt);
+                command.ExecuteNonQuery();
+                connect_to_DB.Close();
+                MessageBox.Show("Andmed uuendatud");
+            }
+            else
+            {
+                MessageBox.Show("Viga");
+            }
+        }
+        TextBox name, date, genre, image;
+        PictureBox posters;
+        DataGridView dataGridView;
+        int id = 0;
+        private void Btn_MouseClick(object sender, MouseEventArgs e)
+        {
+            name = new TextBox
+            {
+                Location = new System.Drawing.Point(100, 200)
+            };
+             date = new TextBox
+            {
+                Location = new System.Drawing.Point(100, 225)
+            };
+             genre = new TextBox
+            {
+                Location = new System.Drawing.Point(100, 250)
+            };
+             image = new TextBox
+            {
+                Location = new System.Drawing.Point(100, 275)
+            };
+            posters = new PictureBox()
+            {
+                Size=new System.Drawing.Size(180,250),
+                Location = new System.Drawing.Point(300, 150)
+            };
+            connect_to_DB.Open();
+            DataTable tabel = new DataTable();
+            dataGridView = new DataGridView();
+            dataGridView.RowHeaderMouseClick += DataGridView_RowHeaderMouseClick1;
+            SqlDataAdapter adapter = new SqlDataAdapter("SELECT ID_movie,name,date,genre FROM [dbo].[Movie]", connect_KinoDB);
+            adapter.Fill(tabel);
+            dataGridView.DataSource = tabel;
+            dataGridView.Location = new System.Drawing.Point(150, 400);
+            dataGridView.Size = new System.Drawing.Size(400, 200);
+            this.Controls.Add(dataGridView);
+           
+            
+            this.Controls.Add(name);
+            this.Controls.Add(date);
+            this.Controls.Add(genre);
+            this.Controls.Add(image);
+            this.Controls.Add(posters);
+            connect_to_DB.Close();
+        }
+
+        private void DataGridView_RowHeaderMouseClick1(object sender, DataGridViewCellMouseEventArgs e)
+        {
+            id = Convert.ToInt32(dataGridView.Rows[e.RowIndex].Cells[0].Value.ToString());
+            name.Text = dataGridView.Rows[e.RowIndex].Cells[1].Value.ToString();
+            date.Text = dataGridView.Rows[e.RowIndex].Cells[2].Value.ToString();
+            genre.Text = dataGridView.Rows[e.RowIndex].Cells[3].Value.ToString();
+            image.Text = dataGridView.Rows[e.RowIndex].Cells[4].Value.ToString();
+            posters.Image = Image.FromFile(@"C:..\..\image\" + dataGridView.Rows[e.RowIndex].Cells[4].Value.ToString());
+            //string v = dataGridView.Rows[e.RowIndex].Cells[5].Value.ToString();
+            //comboBox1.SelectedIndex = Int32.Parse(v) - 1;
+        }
+
+    }
+}
---
+++ cinema.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyVorm
+{
+    public partial class cinema :Form
+    {
+        public static int m = 1;
+        public List<string> listOfMovies;
+        public List<string> listOfMovies_2;
+        Button btn_scroll_back;
+        Button btn_scroll_next;
+        Button choose;
+        Button btn;
+        public PictureBox picture;
+        Button select;
+        public string rt;
+        public Label nameMovie;
+        public cinema()
+        {
+
+            btn = new Button()
+            {
+                Size = new Size(75, 75),
+                Location = new Point(100, 100),
+                Text = "Admin"
+            };
+            this.Controls.Add(btn);
+            btn.MouseClick += Btn_MouseClick;
+            listOfMovies_2 = new List<string>() { "SpongeBob", "Jumanji", "StarWars" };
+            this.Size = new System.Drawing.Size(600,800);
+            this.BackgroundImage = new Bitmap(@"../../image/kos.jpg");
+            Label lbl = new Label()
+            {
+                Text = "Tere tulemast kinno!",
+                Location = new System.Drawing.Point(150, 30),
+                Size = new Size(240, 40),
+                Font = new Font("Arial", 18, FontStyle.Bold),
+                BackColor = Color.FromArgb(39, 61, 167)
+
+            };
+            Label lbl2 = new Label()
+            {
+                Text = "Valige film",
+                Location = new System.Drawing.Point(150, 70),
+                Size = new Size(240, 40),
+                Font = new Font("Arial", 18, FontStyle.Bold),
+                BackColor = Color.FromArgb(39, 61, 167)
+
+            };
+            this.Controls.Add(lbl);
+            this.Controls.Add(lbl2);
+            nameMovie = new Label()
+            {
+                Text = listOfMovies_2[m],
+                Font = new Font(Font.FontFamily, 20),
+                Location = new Point(200, 70),
+                BackColor = Color.Purple,
+                ForeColor = Color.White,
+                AutoSize = true
+            };
+            List<string> lists = new List<string>();
+
+            lists.Add("sb.jpg");
+            lists.Add("zw.jpg");
+            lists.Add("jm.jpg");
+
+            btn_scroll_back = new Button()
+            {
+                Size = new Size(75, 75),
+                Location = new Point(130, 260),
+                Text = "Left"
+            };
+
+            btn_scroll_back.Click += Btn_scroll_back_Click;
+
+
+            btn_scroll_next = new Button()
+            {
+                Size = new Size(75, 75),
+                Location = new Point(350, 260),
+                Text = "Right"
+            };
+
+            btn_scroll_next.Click += Btn_scroll_next_Click;
+
+            /*PictureBox pb = new PictureBox()
+            {
+                ImageLocation = ($"../../image/{lists[0]}"),
+                Location = new Point(25, 200),
+                Size = new Size(150, 250),
+                SizeMode = PictureBoxSizeMode.StretchImage
+                
+            };
+            
+            pb.MouseClick += Pb_MouseClick;*/
+            /*PictureBox pb2 = new PictureBox()
+            {
+                ImageLocation = ($"../../image/{lists[1]}"),
+                Location = new Point(370, 200),
+                Size = new Size(150, 250),
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };*/
+            picture = new PictureBox()
+            {
+                ImageLocation = ($"../../image/{lists[2]}"),
+                Location = new Point(200, 200),
+                Size = new Size(150, 250),
+                SizeMode = PictureBoxSizeMode.StretchImage
+            };
+            /*this.Controls.Add(pb);
+            this.Controls.Add(pb2);*/
+            this.Controls.Add(picture);
+            picture.MouseClick += Picture_MouseClick;
+            /*pb2.MouseClick += Pb2_MouseClick; pb3.MouseClick += Pb3_MouseClick;*/
+
+
+            select = new Button()
+            {
+                Text = "Select movie",
+                Size = new Size(150, 250),
+                Location = new Point(200,400),
+                Font = new Font(Font.FontFamily, 10),
+            };
+
+            
+            select.Hide();
+
+            this.Controls.Add(choose);
+            this.Controls.Add(select);
+            this.Controls.Add(btn_scroll_back);
+            this.Controls.Add(btn_scroll_next);
+
+
+
+            listOfMovies = new List<string>() { "sb.jpg", "jm.jpg", "zw.jpg" };
+
+
+
+
+
+        }
+
+        private void Btn_MouseClick(object sender, MouseEventArgs e)
+        {   
+            admin op = new admin();
+            op.Show();
+        }
+
+        private void Picture_MouseClick(object sender, MouseEventArgs e)
+        {
+            room saal = new room("Valige koht", "", "Väike", "Keskmine", "Suur");
+            saal.StartPosition = FormStartPosition.CenterScreen;
+            saal.ShowDialog();
+        }
+
+        /*private void Select_Click(object sender, EventArgs e)
+        {
+            MyForm uus_aken = new MyForm("Choose a hall", "", "Small", "Middle", "Big", "VIP");
+            uus_aken.StartPosition = FormStartPosition.CenterScreen;
+            uus_aken.ShowDialog();
+        }*/
+
+        public void Btn_scroll_next_Click(object sender, EventArgs e)
+        {
+            rt = M();
+        }
+
+        public void Btn_scroll_back_Click(object sender, EventArgs e)
+        {
+            rt = M1();
+        }
+
+        public string M()
+        {
+            if (m <= 1)
+            {
+                btn_scroll_back.Show();
+                m++;
+                picture.ImageLocation = ($"../../image/{listOfMovies[m]}");
+                nameMovie.Text = listOfMovies_2[m];
+            }
+            if (m == 2)
+            {
+                btn_scroll_next.Hide();
+            }
+
+            return nameMovie.Text;
+        }
+
+        public string M1()
+        {
+            if (m > 0)
+            {
+                btn_scroll_next.Show();
+                m--;
+                picture.ImageLocation = ($"../../image/{listOfMovies[m]}");
+                nameMovie.Text = listOfMovies_2[m];
+            }
+            if (m == 0)
+            {
+                btn_scroll_back.Hide();
+            }
+            return nameMovie.Text;
+        }
+    }
+}
